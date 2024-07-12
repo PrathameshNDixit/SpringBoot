8554af763ecc9128b9aa91571809b57768f0ab44
Prathamesh Dixit <prathameshdixit1234@gmail.com>
2024-07-12 08:37:32 +0530

    week2 2nd:we checked JPA,Entity,Repository,H2 Database

--- src/main/java/com/week2/demo/controllers/EmployeeController.java
+++ src/main/java/com/week2/demo/controllers/EmployeeController.java
@@ -1,26 +1,31 @@
 package com.week2.demo.controllers;
 
 import com.week2.demo.dto.EmployeeDTO;
+import com.week2.demo.entities.EmployeeEntity;
+import com.week2.demo.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
-
+    private final EmployeeRepository emp;
+    EmployeeController(EmployeeRepository emp){
+        this.emp=emp;
+    }
     @GetMapping(path = "/{employeeID}")
-    public EmployeeDTO getEmployeeId(@PathVariable(name = "employeeID") Long id){
-        return new EmployeeDTO(id,"pnd","abc@.com",12, LocalDate.now(),true);
+    public EmployeeEntity getEmployeeId(@PathVariable(name = "employeeID") Long id){
+        return emp.findById(id).orElse(null);
     }
     @GetMapping
-    public String getAllEmployees(@RequestParam(required = false,name = "age") int age){
-        return "All Employees "+age;
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false,name = "age") Integer age){
+        return emp.findAll();
     }
     @PostMapping
-    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO emp){
-        emp.setId(100L);
-        return emp;
+    public EmployeeEntity createEmployee(@RequestBody EmployeeEntity emp){
+        return this.emp.save(emp);
     }
     @PutMapping
     public EmployeeDTO updateEmployee(@RequestBody EmployeeDTO emp){
